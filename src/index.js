50c5cfd234ead5d1d5c95c8d844edf2c7da90f74
Rutik <rutikkulkarni2001@gmail.com>
2024-07-04 14:42:25 +0530

    Implemented responsiveness using MUI and improved component readability and code cleanliness

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css'; 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import History from "./pages/History/History";
+import Home from "./pages/Home/Home";
 
-ReactDOM.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "history",
+        element: <History />,
+      },
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
